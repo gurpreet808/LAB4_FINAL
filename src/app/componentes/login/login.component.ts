e17cb906ae845bf2137cff0f3718eb90305f1ebc
Gurpreet Singh Montenegro <gurpreets808@gmail.com>
2019-11-20 05:08:21 -0300

    Logica de usuario funcionando

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  correo: string;
+  clave: string;
+
+  constructor(public servUsuario: UsuarioService, public router: Router) {
+
+  }
 
   ngOnInit() {
+  }
+
+  loguear(){
+    console.log("Usuario", this.correo);
+    console.log("Clave", this.clave);
+
+    this.servUsuario.loginEmail(this.correo, this.clave);
+  }
+
+  registrar(){
+    this.navegar("/registrarse");
+
+  }
+
+  navegar(url: string){
+    this.router.navigateByUrl(url);
   }
 
 }
--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Usuario } from '../clases/usuario';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,51 @@ export class UsuarioService {
   public logueado = new BehaviorSubject(false);
   el_usuario: Usuario;
   
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth, public db: AngularFireDatabase) {
+    this.afAuth.authState.subscribe(
+      (authState) => {
+        if (authState) {
+          this.logueado.next(true);
+          this.traerDatosDelUsuario(authState.uid);
+        } else {
+          this.logueado.next(false);
+        }
+      }
+    );
+  }
+
+  async loginEmail(correo: string, clave: string) {
+    try {
+      let resultado = await this.afAuth.auth.signInWithEmailAndPassword(correo, clave);
+
+      this.traerDatosDelUsuario(resultado.user.uid);
+
+    } catch (error) {
+      console.log(error.code);
+      if (error.code == "auth/wrong-password") {
+        console.log("Clave incorrecta");
+      }
+      if (error.code == "auth/user-not-found") {
+        console.log("No se encontró ese mail");
+      }
+    }
+  }
+
+  traerDatosDelUsuario(uid: string) {
+    try {
+      let usuarioQuery = this.db.object('/usuarios/' + uid).valueChanges().subscribe(
+        (usuario: Usuario) => {
+          this.el_usuario = usuario;
+          console.log(this.el_usuario);
+          usuarioQuery.unsubscribe();
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
