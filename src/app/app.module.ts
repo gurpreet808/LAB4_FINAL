8db00fac95f310e0bf64085285b6b41c5b565111
Gurpreet Singh Montenegro <gurpreets808@gmail.com>
2019-11-20 04:58:25 -0300

    Agrego Módulo para PrimeNG y limpio app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FIREBASE_CONFIG } from './app.firebase.config';
+import { HttpClientModule } from '@angular/common/http';
+import { PrimeNGModule } from "./modulos/primeng/primeng.module";
+
+//Componentes
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { PanelModule } from 'primeng/panel';
 import { SidebarComponent } from './dashboard/components/sidebar/sidebar.component';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
 import { TableComponent } from './dashboard/components/table/table.component';
-import { TableModule } from 'primeng/table';
-import { InputTextModule } from 'primeng/inputtext';
 import { MenubarComponent } from './dashboard/components/menubar/menubar.component';
-import { MenubarModule } from 'primeng/menubar';
 import { PolarAreaChartComponent } from './dashboard/components/polar-area-chart/polar-area-chart.component';
-import { ChartModule } from 'primeng/chart';
 import { DoughnutChartComponent } from './dashboard/components/doughnut-chart/doughnut-chart.component';
 import { RadarChartComponent } from './dashboard/components/radar-chart/radar-chart.component';
 import { PieChartComponent } from './dashboard/components/pie-chart/pie-chart.component';
 import { LineChartComponent } from './dashboard/components/line-chart/line-chart.component';
-import { ToastModule } from 'primeng/toast';
 import { BarChartComponent } from './dashboard/components/bar-chart/bar-chart.component';
+
 import { MenuComponent } from './componentes/menu/menu.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LoginComponent } from './componentes/login/login.component';
@@ -49,14 +51,13 @@ import { Error404Component } from './componentes/error404/error404.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    PanelModule,
-    SidebarModule,
-    ButtonModule,
-    TableModule,
-    InputTextModule,
-    MenubarModule,
-    ChartModule,
-    ToastModule
+    FormsModule,
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    //AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    HttpClientModule,
+    PrimeNGModule
   ],
   providers: [],
   bootstrap: [AppComponent]
