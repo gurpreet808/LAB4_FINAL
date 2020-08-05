5a4b68e5f2bab3837b1c073862cc90dbcd52d559
gurpreet808 <gurpreets808@gmail.com>
2020-08-05 14:38:10 -0300

    conexión con datos propios

--- src/app/componentes/mi-perfil/mi-perfil.component.ts
+++ src/app/componentes/mi-perfil/mi-perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/clases/usuario';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 @Component({
   selector: 'app-mi-perfil',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mi-perfil.component.css']
 })
 export class MiPerfilComponent implements OnInit {
+  yo: Usuario;
 
-  constructor() { }
+  constructor(public servUsuario:UsuarioService) {
+    servUsuario.el_usuario.subscribe(
+      (_user:Usuario) => {
+        this.yo = _user;
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
