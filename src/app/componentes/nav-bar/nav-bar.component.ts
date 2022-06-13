001598dd2c4c96358d3406cdabe1a08c0b219157
gurpreet808 <gurpreets808@gmail.com>
2022-06-12 22:26:23 -0300

    Agrego navbar

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  items: MenuItem[] = [
+    {
+      label: 'Inicio',
+      icon: 'fa-solid fa-house',
+      routerLink: '/'
+    },
+    {
+      label: 'Salas',
+      icon: 'fa-solid fa-hospital',
+      routerLink: 'salas'
+    },
+    {
+      label: 'Especialidades',
+      icon: 'fa-solid fa-briefcase-medical',
+      routerLink: 'especialidades'
+    },
+    {
+      label: 'Empleados',
+      icon: 'fa-solid fa-users',
+      routerLink: 'empleados'
+    },
+    {
+      label: 'Turnos',
+      icon: 'fa-solid fa-calendar-days',
+      routerLink: 'turnos'
+    },
+    {
+      label: 'Mi perfil',
+      icon: 'fa-solid fa-user-pen',
+      routerLink: 'mi-perfil'
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
