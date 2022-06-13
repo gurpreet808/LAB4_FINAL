2d7ed91707acb08696e6919ed1d855af515176ec
gurpreet808 <gurpreets808@gmail.com>
2022-06-12 22:26:12 -0300

    Agrego componente de spinner

--- src/app/componentes/spinner/spinner.component.ts
+++ src/app/componentes/spinner/spinner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from 'src/app/servicios/spinner.service';
 
 @Component({
   selector: 'app-spinner',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpinnerComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public servSpinner: SpinnerService) {
+  }
+  
   ngOnInit(): void {
   }
 
